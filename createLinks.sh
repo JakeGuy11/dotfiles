082f1a7e0de919dd51c3cbcb518a9a501e405bdd
Jake <Jake_Guy_11@protonmail.ch>
2021-04-20 12:18:20 -0400

    updated link script

--- createLinks.sh
+++ createLinks.sh
@@ -9,4 +9,16 @@ ln -sf ~/github/dotfiles/configs/dunstrc ~/.config/dunst/dunstrc
 echo "Creating vim config..."
 ln -sf ~/github/dotfiles/configs/vimrc ~/.vimrc
 
+echo "Creating bash_profile"
+ln -sf ~/github/dotfiles/configs/bash_profile ~/.bash_profile
+
+echo "Creating bashrc"
+ln -sf ~/github/dotfiles/configs/bashrc ~/.bashrc
+
+echo "Creating gitconfig"
+ln -sf ~/github/dotfiles/configs/gitconfig ~/.gitconfig
+
+echo "Creating xinitrc"
+ln -sf ~/github/dotfiles/configs/xinitrc ~/.xinitrc
+
 echo "Done!"
