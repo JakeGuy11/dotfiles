8f790a17bafcdf12bc7176337cc560cd682b8a2d
Jake <Jake_Guy_11@protonmail.ch>
2021-04-19 20:05:32 -0400

    finished link creation script

--- createLinks.sh
+++ createLinks.sh
@@ -1,1 +1,12 @@
 #!/usr/bin/bash
+echo "Creating Links..."
+echo "Creating i3 config link"
+ln -sf ~/github/dotfiles/configs/i3-config ~/.config/i3/config
+
+echo "Creating dunst config..."
+ln -sf ~/github/dotfiles/configs/dunstrc ~/.config/dunst/dunstrc
+
+echo "Creating vim config..."
+ln -sf ~/github/dotfiles/configs/vimrc ~/.vimrc
+
+echo "Done!"
